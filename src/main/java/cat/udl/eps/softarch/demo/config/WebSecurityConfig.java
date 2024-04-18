16d3e80361d1f88d756c4c3b352ae09b6aa7c4e0
Zihan Chen <zihan1048@live.com>
2024-04-18 11:47:46 +0200

    feat: define signin, signup routes, entrypoint, token filter && allowed origin patterns

--- src/main/java/cat/udl/eps/softarch/demo/config/WebSecurityConfig.java
+++ src/main/java/cat/udl/eps/softarch/demo/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package cat.udl.eps.softarch.demo.config;
 import java.util.Arrays;
 import java.util.List;
 
+import cat.udl.eps.softarch.demo.filter.AuthTokenFilter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.data.repository.query.SecurityEvaluationContextExtension;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -24,10 +26,20 @@ public class WebSecurityConfig {
     @Value("${allowed-origins}")
     String[] allowedOrigins;
 
+    private AuthEntryPointJwt authEntryPointJwt;
+    private AuthTokenFilter authTokenFilter;
+
+    public WebSecurityConfig(AuthEntryPointJwt authEntryPointJwt, AuthTokenFilter authTokenFilter) {
+        this.authEntryPointJwt = authEntryPointJwt;
+        this.authTokenFilter = authTokenFilter;
+    }
+
     @Bean
     protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((auth) -> auth
                 .requestMatchers(HttpMethod.GET, "/identity").authenticated()
+                .requestMatchers(HttpMethod.POST, "/signin").permitAll()
+                .requestMatchers(HttpMethod.POST, "/signup").permitAll()
                 .requestMatchers(HttpMethod.POST, "/users").anonymous()
                 .requestMatchers(HttpMethod.POST, "/users/*").denyAll()
                 .requestMatchers(HttpMethod.DELETE, "/suppliers/*").hasRole("SUPPLIER")
@@ -42,16 +54,20 @@ public class WebSecurityConfig {
                 .requestMatchers(HttpMethod.DELETE, "/**/*").authenticated()
                 .anyRequest().permitAll())
             .csrf((csrf) -> csrf.disable())
+            //.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+            .exceptionHandling().authenticationEntryPoint(authEntryPointJwt).and()
             .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .cors((cors) -> cors.configurationSource(corsConfigurationSource()))
             .httpBasic((httpBasic) -> httpBasic.realmName("tab2kgwiz"));
+        http.addFilterBefore(authTokenFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOriginPatterns(Arrays.asList(allowedOrigins));
+        //corsConfiguration.setAllowedOriginPatterns(Arrays.asList(allowedOrigins));
+        corsConfiguration.setAllowedOriginPatterns(Arrays.asList("*"));
         corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         corsConfiguration.setAllowedHeaders(List.of("*"));
         corsConfiguration.setAllowCredentials(true);
