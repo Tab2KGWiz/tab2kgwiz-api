6d01110380c3ad54e76126ef9f4ee5cc81746b32
Zihan Chen <zihan1048@live.com>
2024-04-18 11:42:50 +0200

    feat: change loadUserByUsername

--- src/main/java/cat/udl/eps/softarch/demo/config/BasicUserDetailsService.java
+++ src/main/java/cat/udl/eps/softarch/demo/config/BasicUserDetailsService.java
@@ -1,5 +1,6 @@
 package cat.udl.eps.softarch.demo.config;
 
+import cat.udl.eps.softarch.demo.domain.User;
 import cat.udl.eps.softarch.demo.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -9,15 +10,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class BasicUserDetailsService implements UserDetailsService {
 
-  final UserRepository userRepository;
+    final UserRepository userRepository;
 
-  public BasicUserDetailsService(UserRepository userRepository) {
-    this.userRepository = userRepository;
-  }
+    public BasicUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
-  @Override
-  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    return userRepository.findById(username)
-        .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
-  }
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findById(username).orElseThrow(() -> new UsernameNotFoundException("User not found with username" + username));
+
+        return BasicUserDetailsImpl.build(user);
+    }
 }
