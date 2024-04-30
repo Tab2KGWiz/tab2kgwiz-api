8b09d146769be72a96dcc4582ae612cefb7d037d
Zihan Chen <zihan1048@live.com>
2024-04-30 10:58:30 +0200

    fix: process builder 1 && 2 execute sequentially

--- src/main/java/cat/udl/eps/softarch/demo/utils/ExternalCommandExecutor.java
+++ src/main/java/cat/udl/eps/softarch/demo/utils/ExternalCommandExecutor.java
@@ -20,6 +20,27 @@ public class ExternalCommandExecutor {
             int exitCode = process.exitValue();
 
             if (exitCode == 0) {
+                ProcessBuilder builder2 = new ProcessBuilder("docker", "run", "--rm", "-v",
+                        "C:\\\\Users\\\\Zihan\\\\Desktop\\\\TFG\\\\tab2kgwiz-api\\\\src\\\\main\\\\resources:/data", "rmlio/rmlmapper-java"
+                        , "-m", "rules.rml.ttl");
+
+                // Redirect standard output (STDOUT) to a file
+                builder2.redirectOutput(new File(
+                        "C:\\Users\\Zihan\\Desktop\\TFG\\tab2kgwiz-api\\src\\main\\resources\\output.txt"));
+
+                try {
+                    Process process2 = builder2.start();
+                    process2.waitFor(); // Wait for the container to finish
+                    int exitCode2 = process.exitValue();
+
+                    if (exitCode2 == 0) {
+                        System.out.println("RML mapper run successfully!");
+                    } else {
+                        System.err.println("Error running RML mapper. Exit code: " + exitCode2);
+                    }
+                } catch (IOException | InterruptedException e) {
+                    e.printStackTrace();
+                }
                 System.out.println("Yarrrml parser run successfully!");
             } else {
                 System.err.println("Error running Yarrrml parser. Exit code: " + exitCode);
@@ -28,27 +49,6 @@ public class ExternalCommandExecutor {
             e.printStackTrace();
         }
 
-        ProcessBuilder builder2 = new ProcessBuilder("docker", "run", "--rm", "-v",
-                "C:\\\\Users\\\\Zihan\\\\Desktop\\\\TFG\\\\tab2kgwiz-api\\\\src\\\\main\\\\resources:/data", "rmlio/rmlmapper-java"
-                , "-m", "rules.rml.ttl");
-
-        // Redirect standard output (STDOUT) to a file
-        builder2.redirectOutput(new File(
-                "C:\\Users\\Zihan\\Desktop\\TFG\\tab2kgwiz-api\\src\\main\\resources\\output.txt"));
-
-        try {
-            Process process = builder2.start();
-            process.waitFor(); // Wait for the container to finish
-            int exitCode = process.exitValue();
-
-            if (exitCode == 0) {
-                System.out.println("RML mapper run successfully!");
-            } else {
-                System.err.println("Error running RML mapper. Exit code: " + exitCode);
-            }
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
 
 //        ProcessBuilder builder2 = new ProcessBuilder("docker", "container", "run", "--rm", "--name", "yarrrmlmapper",
 //                "-v", "C:\\Users\\Zihan\\Desktop\\TFG\\tab2kgwiz-api\\src\\main\\resources:/mnt/data",
