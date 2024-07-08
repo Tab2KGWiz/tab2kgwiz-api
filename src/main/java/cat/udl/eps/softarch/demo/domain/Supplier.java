48f804df0a2d75b1aa992b37c50daa46fd6c6bf7
Zihan Chen <zihan1048@live.com>
2024-07-08 14:58:03 +0200

    fix: forbidden error produces when delete getAuthorities

--- src/main/java/cat/udl/eps/softarch/demo/domain/Supplier.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Supplier.java
@@ -1,11 +1,24 @@
 package cat.udl.eps.softarch.demo.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+
 import jakarta.persistence.Entity;
+
+import java.util.Collection;
 
 @Entity
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class Supplier extends User {
+    @Override
+    @JsonValue(value = false)
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_SUPPLIER");
+    }
 }
