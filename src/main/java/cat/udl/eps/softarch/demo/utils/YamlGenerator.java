9c83aaf2169aa7e1559cc77d3df917b144b9d321
Zihan Chen <zihan1048@live.com>
2024-07-08 12:22:29 +0200

    refactor: yaml generator refactoring

--- src/main/java/cat/udl/eps/softarch/demo/utils/YamlGenerator.java
+++ src/main/java/cat/udl/eps/softarch/demo/utils/YamlGenerator.java
@@ -15,98 +15,117 @@ import java.util.*;
 
 @Component
 public class YamlGenerator {
+    private static final String BASE = "base";
+    private static final String SAREF = "saref";
+    private static final String RDFS = "rdfs";
+    private static final String XSD = "xsd";
+    private static final String S4AGR = "s4agri";
+    private static final String CSV_NAME = "mappings.csv";
+    private static final String RDF_TYPE = "rdf:type";
+    private static final String RDFS_LABEL = "rdfs:label";
 
     public void generateYaml(MappingRepository mappingRepository, ColumnRepository columnRepository, Mapping mapping) throws IOException {
+        Map<String, String> prefixes = initializePrefixes(mapping.getPrefixesURIS());
 
-        Map<String, String> prefixes4Factory = new HashMap<>();
-        prefixes4Factory.put("saref", "https://saref.etsi.org/core/");
-        prefixes4Factory.put("base", "https://tab2kgwiz.udl.cat/");
-        prefixes4Factory.put("rdfs", "http://www.w3.org/2000/01/rdf-schema#");
-        prefixes4Factory.put("xsd", "http://www.w3.org/2001/XMLSchema#");
-        prefixes4Factory.put("s4agri", "https://saref.etsi.org/saref4agri/");
+        YamlMapping yamlMapping = new YamlMapping();
+        yamlMapping.setPrefixes(prefixes);
 
         Map<String, YamlMapping.Mappings> mappings = new HashMap<>();
 
-        ArrayList<String> prefixList = new ArrayList<>(Arrays.asList(mapping.getPrefixesURIS().split(",")));
-
-        prefixList.forEach(prefix -> prefixes4Factory.put(prefix.split(";")[0], prefix.split(";")[1]));
-
-        YamlMapping yamlMapping = new YamlMapping();
-        yamlMapping.setPrefixes(prefixes4Factory);
-
         columnRepository.findByColumnBelongsTo(mapping).forEach(column -> {
             YamlMapping.Mappings yamlMappingsMap = new YamlMapping.Mappings();
-            ArrayList<YamlMapping.PredicateObject> poList = new ArrayList<>();
-            String columnTitle = column.getTitle().replaceAll("[(]", "\\\\(");
-            columnTitle = columnTitle.replaceAll("[)]", "\\\\)");
+            yamlMappingsMap.setSources(Collections.singletonList(new YamlMapping.Sources(CSV_NAME, mapping.getFileFormat())));
 
             if (column.isIdentifier()) {
-                yamlMappingsMap.setSources(List.of(new YamlMapping.Sources("mappings.csv", mapping.getFileFormat())));
-                yamlMappingsMap.setS("base" + ":$(" + columnTitle + ")");
-
-                poList.add(new YamlMapping.PredicateObject("rdf:type", new YamlMapping.PropertyValue(column.getOntologyType()
-                        + "/" + column.getLabel(), "iri")));
-                poList.add(new YamlMapping.PredicateObject("rdfs:label", new YamlMapping.PropertyValue("$("
-                        + columnTitle + ")")));
-
-                if (column.getRelatedTo() != null) {
-                    poList.add(new YamlMapping.PredicateObject("s4agri:isLocatedIn", new YamlMapping.PropertyValue(
-                            column.getRelationShip() + "/$(" + column.getRelatedTo()
-                                    .replaceAll("[(]", "\\\\(")
-                                    .replaceAll("[)]", "\\\\)") + ")", "iri")));
-                }
-
-                yamlMappingsMap.setPo(poList);
+                createIdentifierMappings(yamlMappingsMap, column);
                 mappings.put(column.getTitle().toLowerCase().replaceAll("[\\s()]", ""), yamlMappingsMap);
-
             } else if (column.isMeasurement()) {
-                yamlMappingsMap.setSources(List.of(new YamlMapping.Sources("mappings.csv", mapping.getFileFormat())));
-                poList.add(new YamlMapping.PredicateObject("rdf:type", new YamlMapping.PropertyValue(
-                        "saref:Measurement", "iri")));
-                poList.add(new YamlMapping.PredicateObject("rdfs:label", new YamlMapping.PropertyValue(
-                        column.getLabel())));
-                poList.add(new YamlMapping.PredicateObject("saref:relatesToProperty",
-                        new YamlMapping.PropertyValue(column.getOntologyType(), "iri")));
-
-                YamlMapping.PropertyValue hasValuePropertyValue = new YamlMapping.PropertyValue();
-                hasValuePropertyValue.setValue("$(" + columnTitle + ")");
-                hasValuePropertyValue.setDatatype(column.getDataType());
-                poList.add(new YamlMapping.PredicateObject("saref:hasValue", hasValuePropertyValue));
-
-                poList.add(new YamlMapping.PredicateObject("saref:hasUnit",
-                        new YamlMapping.PropertyValue(column.getHasUnit(), "iri")));
-
-                YamlMapping.PropertyValue hasTimestampPropertyValue = new YamlMapping.PropertyValue();
-                hasTimestampPropertyValue.setValue("$(" + column.getHasTimestamp() + ")");
-                Column hasTimestampColumn = columnRepository.findByTitleAndColumnBelongsTo(column.getHasTimestamp(), mapping);
-                hasTimestampPropertyValue.setDatatype(hasTimestampColumn.getDataType());
-                poList.add(new YamlMapping.PredicateObject("saref:hasTimestamp", hasTimestampPropertyValue));
-
-                poList.add(new YamlMapping.PredicateObject("saref:measurementMadeBy",
-                        new YamlMapping.PropertyValue("base:" + column.getMeasurementMadeBy(), "iri")));
-
-                poList.add(new YamlMapping.PredicateObject("saref:isMeasurementOf",
-                        new YamlMapping.PropertyValue("base" + ":$(" +
-                                column.getIsMeasurementOf() + ")", "iri")));
-
-                yamlMappingsMap.setPo(poList);
+                createMeasurementMappings(columnRepository, yamlMappingsMap, column, mapping);
                 mappings.put(column.getTitle().toLowerCase().replaceAll("[\\s()]", ""), yamlMappingsMap);
             }
         });
-
-
         yamlMapping.setMappings(mappings);
 
-        YAMLFactory yamlFactory = new YAMLFactory();
+        ObjectMapper mapper = createYamlMapper();
+        mapping.setYamlFile(mapper.writeValueAsString(yamlMapping));
+        mappingRepository.save(mapping);
+    }
 
+    private Map<String, String> initializePrefixes(String prefixesURIS) {
+        Map<String, String> prefixes = new HashMap<>();
+        prefixes.put(SAREF, "https://saref.etsi.org/core/");
+        prefixes.put(BASE, "https://tab2kgwiz.udl.cat/");
+        prefixes.put(RDFS, "http://www.w3.org/2000/01/rdf-schema#");
+        prefixes.put(XSD, "http://www.w3.org/2001/XMLSchema#");
+        prefixes.put(S4AGR, "https://saref.etsi.org/saref4agri/");
+
+        Arrays.stream(prefixesURIS.split(","))
+                .map(prefix -> prefix.split(";"))
+                .forEach(parts -> prefixes.put(parts[0], parts[1]));
+
+        return prefixes;
+    }
+
+    private void createIdentifierMappings(YamlMapping.Mappings yamlMappingsMap, Column column) {
+        List<YamlMapping.PredicateObject> poList = new ArrayList<>();
+        String columnTitle = escapeColumnTitle(column.getTitle());
+
+        yamlMappingsMap.setS(BASE + ":$(" + columnTitle + ")");
+        poList.add(new YamlMapping.PredicateObject(RDF_TYPE,
+                new YamlMapping.PropertyValue(column.getOntologyType() + "/" + column.getLabel(), "iri")));
+        poList.add(new YamlMapping.PredicateObject(RDFS_LABEL, new YamlMapping.PropertyValue("$(" + columnTitle + ")")));
+
+        if (column.getRelatedTo() != null) {
+            poList.add(new YamlMapping.PredicateObject("s4agri:isLocatedIn", new YamlMapping.PropertyValue(
+                    column.getRelationShip() + "/$(" + escapeColumnTitle(column.getRelatedTo()) + ")", "iri")));
+        }
+
+        yamlMappingsMap.setPo(poList);
+    }
+
+    private void createMeasurementMappings(ColumnRepository columnRepository, YamlMapping.Mappings yamlMappingsMap,
+                                           Column column, Mapping mapping) {
+        List<YamlMapping.PredicateObject> poList = new ArrayList<>();
+        String columnTitle = escapeColumnTitle(column.getTitle());
+
+        poList.add(new YamlMapping.PredicateObject(RDF_TYPE,
+                new YamlMapping.PropertyValue("saref:Measurement", "iri")));
+        poList.add(new YamlMapping.PredicateObject(RDFS_LABEL, new YamlMapping.PropertyValue(column.getLabel())));
+        poList.add(new YamlMapping.PredicateObject("saref:relatesToProperty",
+                new YamlMapping.PropertyValue(column.getOntologyType(), "iri")));
+
+        YamlMapping.PropertyValue hasValuePropertyValue = new YamlMapping.PropertyValue();
+        hasValuePropertyValue.setValue("$(" + columnTitle + ")");
+        hasValuePropertyValue.setDatatype(column.getDataType());
+        poList.add(new YamlMapping.PredicateObject("saref:hasValue", hasValuePropertyValue));
+
+        poList.add(new YamlMapping.PredicateObject("saref:hasUnit",
+                new YamlMapping.PropertyValue(column.getHasUnit(), "iri")));
+
+        YamlMapping.PropertyValue hasTimestampPropertyValue = new YamlMapping.PropertyValue();
+        hasTimestampPropertyValue.setValue("$(" + column.getHasTimestamp() + ")");
+        Column hasTimestampColumn = columnRepository.findByTitleAndColumnBelongsTo(column.getHasTimestamp(), mapping);
+        hasTimestampPropertyValue.setDatatype(hasTimestampColumn.getDataType());
+        poList.add(new YamlMapping.PredicateObject("saref:hasTimestamp", hasTimestampPropertyValue));
+
+        poList.add(new YamlMapping.PredicateObject("saref:measurementMadeBy",
+                new YamlMapping.PropertyValue(BASE + ":" + column.getMeasurementMadeBy(), "iri")));
+        poList.add(new YamlMapping.PredicateObject("saref:isMeasurementOf",
+                new YamlMapping.PropertyValue(BASE + ":$(" + column.getIsMeasurementOf() + ")", "iri")));
+
+        yamlMappingsMap.setPo(poList);
+    }
+
+    private String escapeColumnTitle(String title) {
+        return title.replaceAll("[(]", "\\\\(").replaceAll("[)]", "\\\\)");
+    }
+
+    private ObjectMapper createYamlMapper() {
+        YAMLFactory yamlFactory = new YAMLFactory();
         // Remove default quotes
         yamlFactory.configure(YAMLGenerator.Feature.MINIMIZE_QUOTES, true);
         // Remove --- from the start of the file
         yamlFactory.configure(YAMLGenerator.Feature.WRITE_DOC_START_MARKER, false);
-
-        ObjectMapper mapper = new ObjectMapper(yamlFactory);
-
-        mapping.setYamlFile(mapper.writeValueAsString(yamlMapping));
-        mappingRepository.save(mapping);
+        return new ObjectMapper(yamlFactory);
     }
 }
