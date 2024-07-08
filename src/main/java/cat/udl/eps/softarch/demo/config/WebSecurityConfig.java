f7f24952d17daa4affe2f2feaa740fe3e93a177c
Zihan Chen <zihan1048@live.com>
2024-07-08 14:32:52 +0200

    refactor: WebSecurityConfig refactoring

--- src/main/java/cat/udl/eps/softarch/demo/config/WebSecurityConfig.java
+++ src/main/java/cat/udl/eps/softarch/demo/config/WebSecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
@@ -26,8 +27,8 @@ public class WebSecurityConfig {
     @Value("${allowed-origins}")
     String[] allowedOrigins;
 
-    private AuthEntryPointJwt authEntryPointJwt;
-    private AuthTokenFilter authTokenFilter;
+    private final AuthEntryPointJwt authEntryPointJwt;
+    private final AuthTokenFilter authTokenFilter;
 
     public WebSecurityConfig(AuthEntryPointJwt authEntryPointJwt, AuthTokenFilter authTokenFilter) {
         this.authEntryPointJwt = authEntryPointJwt;
@@ -35,49 +36,48 @@ public class WebSecurityConfig {
     }
 
     @Bean
-    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((auth) -> auth
-                .requestMatchers(HttpMethod.GET, "/identity").authenticated()
-                .requestMatchers(HttpMethod.POST, "/signin").permitAll()
-                .requestMatchers(HttpMethod.POST, "/signup").permitAll()
-                .requestMatchers(HttpMethod.POST, "/users").anonymous()
-                .requestMatchers(HttpMethod.POST, "/users/*").denyAll()
-                .requestMatchers(HttpMethod.DELETE, "/suppliers/*").hasRole("SUPPLIER")
-                .requestMatchers(HttpMethod.PATCH, "/suppliers/*").hasRole("SUPPLIER")
-                .requestMatchers(HttpMethod.POST, "/suppliers").anonymous()
-                .requestMatchers(HttpMethod.POST, "/mappings").hasRole("SUPPLIER")
-                .requestMatchers(HttpMethod.DELETE, "/mappings/*").hasRole("SUPPLIER")
-                .requestMatchers(HttpMethod.PATCH, "/mappings/{id}").hasRole("SUPPLIER")
-                .requestMatchers(HttpMethod.POST, "/mappings/{id}/columns").hasRole("SUPPLIER")
-                .requestMatchers(HttpMethod.PUT, "/mappings/{id}/columns/{columnId}").hasRole("SUPPLIER")
-                .requestMatchers(HttpMethod.DELETE, "/columns/*").hasRole("SUPPLIER")
-                .requestMatchers(HttpMethod.PATCH, "/columns/*").hasRole("SUPPLIER")
-                .requestMatchers(HttpMethod.POST, "/mappings/{id}/yaml/generate").hasRole("SUPPLIER")
-                .requestMatchers(HttpMethod.GET, "/mappings/{id}/columns").hasRole("SUPPLIER")
-                .requestMatchers(HttpMethod.POST, "/mappings/{id}/generate").permitAll()
-                //.requestMatchers(HttpMethod.POST, "/**/*").hasRole("SUPPLIER")
-                .requestMatchers(HttpMethod.PUT, "/**/*").authenticated()
-                .requestMatchers(HttpMethod.PATCH, "/**/*").authenticated()
-                .requestMatchers(HttpMethod.DELETE, "/**/*").authenticated()
-                .anyRequest().permitAll())
-            .csrf((csrf) -> csrf.disable())
-            //.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
-            .exceptionHandling().authenticationEntryPoint(authEntryPointJwt).and()
-            .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .cors((cors) -> cors.configurationSource(corsConfigurationSource()));
-            //.httpBasic((httpBasic) -> httpBasic.realmName("tab2kgwiz"));
-        http.addFilterBefore(authTokenFilter, UsernamePasswordAuthenticationFilter.class);
-        return http.build();
+    protected SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers(HttpMethod.GET, "/identity").authenticated()
+                        .requestMatchers(HttpMethod.POST, "/signin").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/signup").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/users").anonymous()
+                        .requestMatchers(HttpMethod.POST, "/users/*").denyAll()
+                        .requestMatchers(HttpMethod.DELETE, "/suppliers/*").hasRole("SUPPLIER")
+                        .requestMatchers(HttpMethod.PATCH, "/suppliers/*").hasRole("SUPPLIER")
+                        .requestMatchers(HttpMethod.POST, "/suppliers").anonymous()
+                        .requestMatchers(HttpMethod.POST, "/mappings").hasRole("SUPPLIER")
+                        .requestMatchers(HttpMethod.DELETE, "/mappings/*").hasRole("SUPPLIER")
+                        .requestMatchers(HttpMethod.PATCH, "/mappings/{id}").hasRole("SUPPLIER")
+                        .requestMatchers(HttpMethod.POST, "/mappings/{id}/columns").hasRole("SUPPLIER")
+                        .requestMatchers(HttpMethod.PUT, "/mappings/{id}/columns/{columnId}").hasRole("SUPPLIER")
+                        .requestMatchers(HttpMethod.DELETE, "/columns/*").hasRole("SUPPLIER")
+                        .requestMatchers(HttpMethod.PATCH, "/columns/*").hasRole("SUPPLIER")
+                        .requestMatchers(HttpMethod.POST, "/mappings/{id}/yaml/generate").hasRole("SUPPLIER")
+                        .requestMatchers(HttpMethod.GET, "/mappings/{id}/columns").hasRole("SUPPLIER")
+                        .requestMatchers(HttpMethod.POST, "/mappings/{id}/generate").permitAll()
+                        .requestMatchers(HttpMethod.PUT, "/**/*").authenticated()
+                        .requestMatchers(HttpMethod.PATCH, "/**/*").authenticated()
+                        .requestMatchers(HttpMethod.DELETE, "/**/*").authenticated()
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling((exceptionHandling) -> exceptionHandling.authenticationEntryPoint(authEntryPointJwt))
+                .sessionManagement((sessionManagement) -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .cors((cors) -> cors.configurationSource(corsConfigurationSource()))
+                .addFilterBefore(authTokenFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        //corsConfiguration.setAllowedOriginPatterns(Arrays.asList(allowedOrigins));
-        corsConfiguration.setAllowedOriginPatterns(Arrays.asList("*"));
+        corsConfiguration.setAllowedOriginPatterns(List.of("*"));
         corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         corsConfiguration.setAllowedHeaders(List.of("*"));
         corsConfiguration.setAllowCredentials(true);
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfiguration);
         return source;
