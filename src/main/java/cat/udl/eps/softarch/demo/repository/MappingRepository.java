e0bb89fb6049539c2665c5c98a00c13e0dcadee2
Zihan Chen <zihan1048@live.com>
2024-07-08 13:51:37 +0200

    refactor: delete custom column projection

--- src/main/java/cat/udl/eps/softarch/demo/repository/MappingRepository.java
+++ src/main/java/cat/udl/eps/softarch/demo/repository/MappingRepository.java
@@ -10,9 +10,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 @RepositoryRestResource(excerptProjection = CustomMapping.class)
