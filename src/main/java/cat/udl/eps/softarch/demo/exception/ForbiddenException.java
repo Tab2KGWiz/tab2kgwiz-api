1fdf55eded8de12a3283136079d0a9c750a54702
Zihan Chen <zihan1048@live.com>
2024-07-08 13:52:56 +0200

    feat: ForbiddenException && NotAuthorizedException impl

--- src/main/java/cat/udl/eps/softarch/demo/exception/ForbiddenException.java
+++ src/main/java/cat/udl/eps/softarch/demo/exception/ForbiddenException.java
@@ -3,6 +3,9 @@ package cat.udl.eps.softarch.demo.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value= HttpStatus.FORBIDDEN, reason = "Unauthorized")
-public class ForbiddenException extends RuntimeException{
+@ResponseStatus(value = HttpStatus.FORBIDDEN, reason = "Unauthorized")
+public class ForbiddenException extends RuntimeException {
+    public ForbiddenException() {
+        super("Unauthorized");
+    }
 }
--- src/main/java/cat/udl/eps/softarch/demo/exception/NotAuthorizedException.java
+++ src/main/java/cat/udl/eps/softarch/demo/exception/NotAuthorizedException.java
@@ -3,6 +3,9 @@ package cat.udl.eps.softarch.demo.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value= HttpStatus.UNAUTHORIZED, reason = "Authorization required")
-public class NotAuthorizedException extends RuntimeException{
+@ResponseStatus(value = HttpStatus.UNAUTHORIZED, reason = "Authorization required")
+public class NotAuthorizedException extends RuntimeException {
+    public NotAuthorizedException() {
+        super("Authorization required");
+    }
 }
